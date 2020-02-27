847318f5e4617674ea1ea2fec57c4191b791e905
Tyler Kummer <tylerkummer@protonmail.com>
2020-02-27 15:51:29 -0600

    Displayed form to the DOM

--- my-app/src/OnboardForm.js
+++ my-app/src/OnboardForm.js
@@ -6,18 +6,28 @@ import axios from "axios";
 const OnboardForm =({values, errors, touched, status}) => {
     return (
         <div className ="onboard">
-            <Form>
-                <label htmlFor="onboard-name">Name </label>
-                <Field name="onboard-name" type="text"/>
+            <Form style={{display: "flex", flexDirection: "column", margin: "0 auto", width: "25%"}}>
+                <label htmlFor="name">Name </label>
+                <Field name="name" type="text" placeholder="name"/>
+                <label htmlFor="email">Email </label>
+                <Field name="email" type="email" placeholder="email"/>
+                <label htmlFor="password">Password </label>
+                <Field name="password" type="password" placeholder="password"/>
+                <label htmlFor="checkbox">Terms of Service </label>
+                <Field name="checkbox" type="checkbox" placeholder="TOS"/>
+                <button type="submit">Submit!</button>
             </Form>
         </div>
     )
 }
 
 const FormikOnboardForm = withFormik({
-    mapPropsToValues({name}){
+    mapPropsToValues({name, email, password, checkbox}){
         return {
-            name: name || ""
+            name: name || "",
+            email: email || "",
+            password: password || "",
+            checkbox: checkbox || false
         }
     }
 })(OnboardForm);
